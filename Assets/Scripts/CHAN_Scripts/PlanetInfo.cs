891984eda378faf5de526bca869752dded5b3f82
kwonyoungchan <kyckyc0530@gmail.com>
2022-10-11 14:00:24 +0900

    aa

--- Assets/Scripts/CHAN_Scripts/PlanetInfo.cs
+++ Assets/Scripts/CHAN_Scripts/PlanetInfo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlanetInfo :MonoBehaviour
 {
     public int PlanetCount=400;
+    // 기준 반경을 설정
     public float radius=250;
     [HideInInspector]public float[] eachSpeed;
     public float rotateSpeed=1.5f;
@@ -12,4 +13,27 @@ public class PlanetInfo :MonoBehaviour
     public float maxScale=10;
 
     public List<GameObject> planets = new List<GameObject>();
+
+    // 반경값 제어 함수부
+    public void ControlRadius()
+    {
+        // 슬라이더 초기 위치는 중간
+        // 스크롤바가 왼쪽으로 가면 반경 감소 
+        // 스크롤바가 오른쪽으로 가면 반경 증가 
+    }
+    // 행성 갯수 제어 함수부
+    public void ControlNOP()
+    {
+
+    }
+    //행성 크기 제어 함수부
+    public void ControlScale()
+    {
+
+    }
+    //행성 공전속도 제어 함수부
+    public void ControlSpeed()
+    {
+
+    }
 }
--- Assets/Scripts/CHAN_Scripts/SpawnManager.cs
+++ Assets/Scripts/CHAN_Scripts/SpawnManager.cs
@@ -12,7 +12,7 @@ public class SpawnManager : MonoBehaviour
     bool turn;
     void Start()
     {
-      
+
     }
     //공전하는 효과를 추가하자S
     // 게임씬에서 크기, 속도, 반경을 바꿀 수 있도록 한다.
@@ -20,36 +20,49 @@ public class SpawnManager : MonoBehaviour
     {
         if (!turn)
         {
-            StartCoroutine(initialize());
+
+            info.eachSpeed = new float[info.PlanetCount];
+            float curTime = 0;
+            for (int i = 0; i < info.PlanetCount; i++)
+            {
+                while (curTime < 1f)
+                {
+                    curTime += Time.deltaTime;
+                }
+                curTime = 0;
+                GameObject p = Instantiate(Resources.Load<GameObject>("CHAN_Resources/planet"), gameObject.transform);
+                p.transform.localScale = p.transform.localScale * Random.Range(info.minScale, info.maxScale);
+                info.planets.Add(p);
+                p.transform.localPosition = Random.insideUnitSphere * info.radius;
+                Material _material = p.GetComponent<Renderer>().material;
+                _material.SetColor("_EmissionColor", PlanetColor.GetColor("_EmissionColor"));
+                info.eachSpeed[i] = Time.deltaTime * Random.Range(0.2f, info.rotateSpeed);
+
+            }
             turn = true;
         }
-        
+
         for (int i = 0; i < info.planets.Count; i++)
         {
             info.planets[i].transform.RotateAround(transform.position, transform.up, info.eachSpeed[i]);
         }
     }
-
-    IEnumerator initialize()
-    {
-        info.eachSpeed = new float[info.PlanetCount];
-        //count만큼 행성을 생성한다.
-        // 딜레이를 줘보자
-
-        for (int i = 0; i < info.PlanetCount; i++)
-        {
-            yield return new WaitForSeconds(0.2f);
-            GameObject p = Instantiate(Resources.Load<GameObject>("CHAN_Resources/planet"), gameObject.transform);
-            p.transform.localScale = p.transform.localScale * Random.Range(info.minScale, info.maxScale);
-            info.planets.Add(p);
-            p.transform.localPosition = Random.insideUnitSphere * info.radius;
-            Material _material = p.GetComponent<Renderer>().material;
-            _material.SetColor("_EmissionColor", PlanetColor.GetColor("_EmissionColor"));
-            info.eachSpeed[i] = Time.deltaTime * Random.Range(0.2f, info.rotateSpeed);
-
-        }
-        
+    // 반경값 제어 함수부
+    public void ControlRadius()
+    { 
+    
     }
+    // 행성 갯수 제어 함수부
+    public void ControlNOP()
+    {
+
+    }
+    //행성 크기 제어 함수부
+    public void ControlScale()
+    { 
+    
+    }
+
 
 
 }
