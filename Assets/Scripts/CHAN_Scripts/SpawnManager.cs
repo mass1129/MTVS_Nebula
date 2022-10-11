1020749e088b7b3bac339055e798392e405a0cab
kwonyoungchan <kyckyc0530@gmail.com>
2022-10-11 13:04:36 +0900

    K

--- Assets/Scripts/CHAN_Scripts/SpawnManager.cs
+++ Assets/Scripts/CHAN_Scripts/SpawnManager.cs
@@ -9,27 +9,47 @@ public class SpawnManager : MonoBehaviour
     // 각 카테고리마다 색을 다르게 한다. 
     PlanetInfo info = new PlanetInfo();
     public Material PlanetColor;
+    bool turn;
     void Start()
     {
-        info.eachSpeed = new float[info.PlanetCount];
-        //count만큼 행성을 생성한다.
-        for (int i = 0; i < info.PlanetCount; i++)
-        {
-            GameObject p = Instantiate(Resources.Load<GameObject>("CHAN_Resources/planet"));
-            p.transform.localScale =p.transform.localScale*Random.Range(info.minScale, info.maxScale);
-            info.planets.Add(p);
-            p.transform.position = transform.position + Random.insideUnitSphere * info.radius;
-            Material _material = p.GetComponent<Renderer>().material;
-            _material.SetColor("_EmissionColor", PlanetColor.GetColor("_EmissionColor"));
-            info.eachSpeed[i] = Time.deltaTime * Random.Range(0.2f, info.rotateSpeed);
-        }
+      
     }
     //공전하는 효과를 추가하자S
+    // 게임씬에서 크기, 속도, 반경을 바꿀 수 있도록 한다.
     void Update()
     {
+        if (!turn)
+        {
+            StartCoroutine(initialize());
+            turn = true;
+        }
+        
         for (int i = 0; i < info.planets.Count; i++)
         {
             info.planets[i].transform.RotateAround(transform.position, transform.up, info.eachSpeed[i]);
         }
     }
+
+    IEnumerator initialize()
+    {
+        info.eachSpeed = new float[info.PlanetCount];
+        //count만큼 행성을 생성한다.
+        // 딜레이를 줘보자
+
+        for (int i = 0; i < info.PlanetCount; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            GameObject p = Instantiate(Resources.Load<GameObject>("CHAN_Resources/planet"), gameObject.transform);
+            p.transform.localScale = p.transform.localScale * Random.Range(info.minScale, info.maxScale);
+            info.planets.Add(p);
+            p.transform.localPosition = Random.insideUnitSphere * info.radius;
+            Material _material = p.GetComponent<Renderer>().material;
+            _material.SetColor("_EmissionColor", PlanetColor.GetColor("_EmissionColor"));
+            info.eachSpeed[i] = Time.deltaTime * Random.Range(0.2f, info.rotateSpeed);
+
+        }
+        
+    }
+
+
 }
