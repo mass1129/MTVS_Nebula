efc2dc1522c02872cf2733e4276d3e4da84c91e3
kwonyoungchan <kyckyc0530@gmail.com>
2022-10-30 21:56:15 +0900

    a

--- Assets/Scripts/CHAN_Scripts/forProto/Item/Item_Detector.cs
+++ Assets/Scripts/CHAN_Scripts/forProto/Item/Item_Detector.cs
@@ -19,18 +19,14 @@ public class Item_Detector : MonoBehaviour
         {
             if (c.gameObject.CompareTag("TV"))
             {
-
                 //여기서 감지가 되면 TV 앞에 안내 Text 나오게 한다.
-                Item_TVManager.instance.CanControlTV(true);
-                Item_TVManager.instance.done = true;
+                
             }
         }
         if (detect.Length == 0)
         {
-            Item_TVManager.instance.CanControlTV(false);
-            Item_TVManager.instance.done = false;
-            if (Item_TVManager.instance.isTurn)
-                Copy_Window_Texture.instance.OnClicked(false);
+            
         }
     }
 }
+
