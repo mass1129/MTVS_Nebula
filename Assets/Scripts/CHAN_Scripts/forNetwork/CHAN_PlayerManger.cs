79e27f6b90b23d4a8f18de636cce2c7556a7413f
kwonyoungchan <kyckyc0530@gmail.com>
2022-11-07 13:52:26 +0900

    a

--- Assets/Scripts/CHAN_Scripts/forNetwork/CHAN_PlayerManger.cs
+++ Assets/Scripts/CHAN_Scripts/forNetwork/CHAN_PlayerManger.cs
@@ -9,16 +9,20 @@ public class CHAN_PlayerManger : MonoBehaviourPun
     private void Awake()
     {
         // 캐릭터가 자기자신일 때 저장
-        if (photonView.IsMine)
-        { 
-            CHAN_PlayerManger.LocalPlayerInstance = this.gameObject;
-            transform.GetChild(1).GetComponent<IconMove>().OnPlayerIcon();
-        }
-        else
+        if (CHAN_GameManager.instance.prefab == CHAN_GameManager.instance.WhalePrepab)
         {
-            gameObject.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
-            transform.GetChild(1).GetComponent<IconMove>().OnUserIcon();
+            if (photonView.IsMine)
+            {
+                CHAN_PlayerManger.LocalPlayerInstance = this.gameObject;
+                transform.GetChild(1).GetComponent<IconMove>().OnPlayerIcon();
+            }
+            else
+            {
+                gameObject.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+                transform.GetChild(1).GetComponent<IconMove>().OnUserIcon();
+            }
         }
+        
         DontDestroyOnLoad(gameObject);
         CHAN_ClientManager.instance.AddPlayer(this.photonView);
     }
