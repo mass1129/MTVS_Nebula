6084926f6322d183e610cced02deeec000f229c8
kwonyoungchan <kyckyc0530@gmail.com>
2022-11-10 20:53:07 +0900

    d

--- Assets/Scripts/CHAN_Scripts/User/User/Test_UserProfile.cs
+++ Assets/Scripts/CHAN_Scripts/User/User/Test_UserProfile.cs
@@ -12,7 +12,10 @@ public class Test_UserProfile : MonoBehaviour
         instance = this;
     }
     // 유저정보 정상적으로 로드 되는지 테스트  
-
+    public void OnClickBtn()
+    {
+        Load();
+    }
     public void Save(Profile_Info Container)
     {
 
@@ -22,15 +25,76 @@ public class Test_UserProfile : MonoBehaviour
         K_SaveSystem.Save("ProfileSave", json, true);
 
     }
-    public void Load(Profile_Info Container)
+    public async void Load()
     {
-        if (PlayerPrefs.HasKey("ProfileSave"))
+        List<Result> values = new List<Result>();
+        var url = "http://ec2-43-201-55-120.ap-northeast-2.compute.amazonaws.com:8001/avatar";
+        var httpRequest = new HttpRequester(new JsonSerializationOption());
+        Root result = await httpRequest.Get<Root>(url);
+
+        values = result.results;
+        foreach (Result i in values)
         {
-            string json = PlayerPrefs.GetString("ProfileSave");
-            json = K_SaveSystem.Load("ProfileSave");
-
-            JsonUtility.FromJsonOverwrite(json, Container);
-
+            print(i.id);
+            print(i.name);
+            print(i.imageUrl);
+            print(i.hashTags);
+            print(i.skyIslandId);
         }
+        
     }
+    public class Blendshape
+    {
+        public string blendshapeName { get; set; }
+        public int type { get; set; }
+        public double value { get; set; }
+        public int group { get; set; }
+    }
+
+    public class Result
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string imageUrl { get; set; }
+        public List<string> hashTags { get; set; }
+        public int skyIslandId { get; set; }
+        public Texture texture { get; set; }
+    }
+
+    public class Root
+    {
+        public int httpStatus { get; set; }
+        public string message { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+    public class SelectedElements
+    {
+        public int Hair { get; set; }
+        public int Beard { get; set; }
+        public int Accessory { get; set; }
+        public int Shirt { get; set; }
+        public int Item1 { get; set; }
+        public int Pants { get; set; }
+        public int Hat { get; set; }
+        public int Shoes { get; set; }
+    }
+
+    public class Texture
+    {
+        public List<double> TeethColor { get; set; }
+        public List<double> OralCavityColor { get; set; }
+        public SelectedElements selectedElements { get; set; }
+        public List<double> HairColor { get; set; }
+        public int MaxLod { get; set; }
+        public int MinLod { get; set; }
+        public List<double> EyeColor { get; set; }
+        public List<double> SkinColor { get; set; }
+        public List<double> UnderpantsColor { get; set; }
+        public string settingsName { get; set; }
+        public double HeadSize { get; set; }
+        public double Height { get; set; }
+        public List<Blendshape> blendshapes { get; set; }
+    }
+
 }
--- Assets/Scripts/CHAN_Scripts/User/User/User_Move.cs
+++ Assets/Scripts/CHAN_Scripts/User/User/User_Move.cs
@@ -6,38 +6,75 @@ using UnityEngine;
 /// 커맨드 값 : WASD , Shift
 /// </summary>
 
-public class User_Move : MonoBehaviourPun,IPunObservable
+public class User_Move : MonoBehaviourPun, IPunObservable
 {
     private void Awake()
     {
         DontDestroyOnLoad(this);
     }
     public float userSpeed;
+    public float rotateSpeed;
     public float speedMultiplier;
 
 
     void Start()
     {
-        
-    }
 
-    
+    }
     Vector3 dir;
     float lerpSpeed = 10;
     public bool islandSelected;
 
     Vector3 receivePos;
+    Quaternion receiveRot;
+    //W: 전진
+    //S: 후진 
+    //A: 반시계방향 회전 
+    //D: 시계방향 회전
+    //MouseUp : 고래 위로 전환
+    //MouseDown : 고래 아래로 전환
+    //Shift: 대쉬
+    //Space bar : 고래 울음소리
+    float _Input_Forward;
+    float _Input_Rotate_Yaw;
+    float _Input_Rotate_Pitch;
 
+    #region 플레이어 입력기
+    float Input_Forward
+    {
+        get { return _Input_Forward; }
+        set { _Input_Forward = Input.GetAxis("Vertical"); }
+    }
+
+    float Input_Rotate_Yaw
+    {
+        get { return _Input_Rotate_Yaw; }
+        set
+        { _Input_Rotate_Yaw = Input.GetAxis("Horizontal"); }
+    }
+    float Input_Rotate_Pitch
+    { get { return _Input_Rotate_Pitch; }
+        set { _Input_Rotate_Pitch = Input.GetAxis("Mouse Y"); }
+    }
+        #endregion
+    
+    float Rotate_Pitch;
+    float Rotate_Yaw;
+
+    //특수효과
+    //유저들끼리 가까이 전근해 있을때 이동속도가 증가하게 됨.
+
+    [System.Obsolete]
     void Update()
     {
 
+        
         if (photonView.IsMine)
         {
-            float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
-
-
-            dir = (/*(x* transform.right) +*/ (y * transform.forward)).normalized;
+            // 일단 이동방향은 앞뒤로 갈 수 있도록 만든다. 
+            dir = (Input_Forward * transform.forward).normalized;
+            Rotate_Pitch -= Input_Rotate_Pitch * rotateSpeed * Time.deltaTime;
+            Rotate_Yaw += Input_Rotate_Yaw * rotateSpeed * Time.deltaTime;
             //쉬프트키를 눌렀을 때 대쉬되도록 만든다.
             float totalSpeed;
             if (Input.GetKey(KeyCode.LeftShift))
@@ -48,13 +85,13 @@ public class User_Move : MonoBehaviourPun,IPunObservable
             {
                 totalSpeed = userSpeed;
             }
-
-
             transform.position += dir * totalSpeed * Time.deltaTime;
+            transform.localRotation = Quaternion.EulerAngles(Rotate_Pitch, Rotate_Yaw, 0);
         }
         else
         {
             transform.position = Vector3.Lerp(transform.position, receivePos, lerpSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, receiveRot, lerpSpeed * Time.deltaTime);
         }
         
     }
@@ -64,10 +101,12 @@ public class User_Move : MonoBehaviourPun,IPunObservable
         if (stream.IsWriting)
         {
             stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
         }
         else if (stream.IsReading)
         {
             receivePos=(Vector3)stream.ReceiveNext();
+            receiveRot = (Quaternion)stream.ReceiveNext();
         }
     }
 }
