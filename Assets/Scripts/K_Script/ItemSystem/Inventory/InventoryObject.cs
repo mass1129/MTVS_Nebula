6c616a2caca640e4469009fd18dca58358a56cbb
mass1129 <quality4132@gmail.com>
2023-01-10 22:24:46 +0900

    feature : 인벤토리에 아이템 얻는 메소드와 클라이언트에 저장 할 수 있도록 저장 메소드 수정 #9

--- Assets/Scripts/K_Script/ItemSystem/Inventory/InventoryObject.cs
+++ Assets/Scripts/K_Script/ItemSystem/Inventory/InventoryObject.cs
@@ -8,7 +8,7 @@ public class InventoryObject : ScriptableObject
     public string loadPath;
     public ItemDatabaseObject database;
     public InterfaceType type;
-
+    public string fileName;
     [SerializeField]
     private Inventory Container = new Inventory();
     //private로 처리된 Container에 접근하기위한 읽기 전용 슬롯
@@ -64,32 +64,43 @@ public class InventoryObject : ScriptableObject
 
 
 
-    public async UniTask InventorySave(string s)
+    public async UniTaskVoid InventorySave(string s, bool fileSave= true)
     {
 
         string json = JsonUtility.ToJson(Container, true);
-        Debug.Log(json);
-        //string s = PlayerPrefs.GetString(" AvatarName");
+        Debug.Log(json);    
+        if(fileSave)
+        {
+            K_SaveSystem.Save(fileName, json, true);
+            return;
+        }
         var url = "https://resource.mtvs-nebula.com/" + savePath + s;
         var httpReq = new HttpRequester(new JsonSerializationOption());
 
         await httpReq.Post(url, json);
     }
 
-    public async UniTask InventoryLoad(string s)
-    {
+    public async UniTaskVoid InventoryLoad(string s, bool fileSave = true)
+    {   
+        if(fileSave)
+        {
+            Inventory result = K_SaveSystem.LoadObject<Inventory>(fileName);
+            for (int i = 0; i < Container.slots.Length; i++)
+            {
+                Container.slots[i].UpdateSlot(result.slots[i].item, result.slots[i].amount);
+            }
+            return;
+        }    
         var url = "https://resource.mtvs-nebula.com/" + loadPath + s;
         var httpReq = new HttpRequester(new JsonSerializationOption());
-        //스택 변수 = 힙 참조 값
+        //스택 변수 = 힙에 참조 값 생성
         H_I_Root result2 = await httpReq.Get<H_I_Root>(url);
-        for (int i = 0; i < Container.slots.Length; i++)
-        {
-            Container.slots[i].UpdateSlot(result2.results.slots[i].item, result2.results.slots[i].amount);
-        }
-        //코드블록 종료시 스택 자동 삭제 -> 힙의 데이터를 참조하는 스택 데이터가 삭제되어 가비지콜렉터가 힙에 저장된 데이터 삭제  
+        this.Container = result2.results;
+        UpdateInventory();
+        //코드블록 종료시 스택(result2) 자동 삭제 -> 힙의 데이터를 참조하는 스택(result2)가 삭제되었지만 플레이어 인벤토리가 참조하고있어서 힙 유지
     }
 
-    public async UniTask ForGiveItem(InventorySlot dropSlot, string avatarName)
+    public async UniTaskVoid ForGiveItem(InventorySlot dropSlot, string avatarName)
     {
         DropItem dropItem = new DropItem
         {
@@ -97,15 +108,14 @@ public class InventoryObject : ScriptableObject
         };
         if (dropItem.uniqueId < 0) return;
         string json = JsonUtility.ToJson(dropItem, true);
-        Debug.Log(json);
         var url = "https://resource.mtvs-nebula.com/achieve/drop/" + avatarName;
         var httpReq = new HttpRequester(new JsonSerializationOption());
 
         await httpReq.Post(url, json);
 
-        await InventoryLoad(avatarName);
+        InventoryLoad(avatarName).Forget();
     }
-   
+
 
 
 
@@ -118,78 +128,78 @@ public class InventoryObject : ScriptableObject
     }
 
     #region 주석처리
-    //public bool AddItem(Item item, int amount)
-    //{
-    //    //빈 슬룻이 없다면 false리턴
-    //    if (EmptySlotCount <= 0)
-    //        return false;
-    //    //
-    //    InventorySlot slot = FindItemOnInventory(item);
-    //    if (!database.ItemObjects[item.id].stackable || slot == null)
-    //    {
-    //        GetEmptySlot().UpdateSlot(item, amount);
-    //        return true;
-    //    }
-    //    slot.AddAmount(amount);
-    //    return true;
-    //}
-    //public bool IsItemInInventory(ItemObject item)
-    //{
-    //    for (int i = 0; i < Container.slots.Length; i++)
-    //    {
-    //        if (Container.slots[i].item.id == item.data.id)
-    //        {
-    //            return true;
-    //        }
-    //    }
-    //    return false;
-    //}
+    public bool AddItem(Item item, int amount)
+    {
+        //빈 슬룻이 없다면 false리턴
+        if (EmptySlotCount <= 0)
+            return false;
+        //
+        InventorySlot slot = FindItemOnInventory(item);
+        if (!database.ItemObjects[item.id].stackable || slot == null)
+        {
+            GetEmptySlot().UpdateSlot(item, amount);
+            return true;
+        }
+        slot.AddAmount(amount);
+        return true;
+    }
+    public bool IsItemInInventory(ItemObject item)
+    {
+        for (int i = 0; i < Container.slots.Length; i++)
+        {
+            if (Container.slots[i].item.id == item.data.id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    //public int EmptySlotCount
-    //{
-    //    get
-    //    {
-    //        int counter = 0;
-    //        for (int i = 0; i < Container.slots.Length; i++)
-    //        {
-    //            if (Container.slots[i].item.id <= -1)
-    //            {
-    //                counter++;
-    //            }
+    public int EmptySlotCount
+    {
+        get
+        {
+            int counter = 0;
+            for (int i = 0; i < Container.slots.Length; i++)
+            {
+                if (Container.slots[i].item.id <= -1)
+                {
+                    counter++;
+                }
 
-    //        }
-    //        return counter;
-    //    }
-    //}
+            }
+            return counter;
+        }
+    }
 
-    ////인벤토리 슬롯에 해당 item이 있는 슬롯을 찾아서 해당 slot를 리턴한다.
-    //public InventorySlot FindItemOnInventory(Item item)
-    //{
-    //    //
-    //    for (int i = 0; i < Container.slots.Length; i++)
-    //    {
-    //        if (Container.slots[i].item.id == item.id)
-    //        {
-    //            return Container.slots[i];
-    //        }
+    //인벤토리 슬롯에 해당 item이 있는 슬롯을 찾아서 해당 slot를 리턴한다.
+    public InventorySlot FindItemOnInventory(Item item)
+    {
+        //
+        for (int i = 0; i < Container.slots.Length; i++)
+        {
+            if (Container.slots[i].item.id == item.id)
+            {
+                return Container.slots[i];
+            }
 
-    //    }
-    //    return null;
+        }
+        return null;
 
-    //}
+    }
 
 
-    //public InventorySlot GetEmptySlot()
-    //{
-    //    for (int i = 0; i < Container.slots.Length; i++)
-    //    {
-    //        if (Container.slots[i].item.id <= -1)
-    //        {
-    //            return Container.slots[i];
-    //        }
-    //    }
-    //    return null;
-    //}
+    public InventorySlot GetEmptySlot()
+    {
+        for (int i = 0; i < Container.slots.Length; i++)
+        {
+            if (Container.slots[i].item.id <= -1)
+            {
+                return Container.slots[i];
+            }
+        }
+        return null;
+    }
     #endregion 
 }
 
